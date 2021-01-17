769b1751705a87b279c041ce12a0d36d4b8453c4
Thanos <thanasakis@gmail.com>
2021-01-17 22:57:29 +0100

    improve style, add.rounded corners

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -8,15 +8,25 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(_product.imageUrl, fit: BoxFit.cover),
-      footer: GridTileBar(
-        leading: IconButton(icon: Icon(Icons.favorite),onPressed: () {}),
-        trailing: IconButton(icon: Icon(Icons.shopping_cart),onPressed: () {}),
-        backgroundColor: Colors.black54,
-        title: Text(
-          _product.title,
-          textAlign: TextAlign.center,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(_product.imageUrl, fit: BoxFit.cover),
+        footer: GridTileBar(
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
+          ),
+          trailing: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {},
+              color: Theme.of(context).accentColor),
+          backgroundColor: Colors.black87,
+          title: Text(
+            _product.title,
+            textAlign: TextAlign.center,
+          ),
         ),
       ),
     );
