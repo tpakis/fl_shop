bbffe36e9d664659bec916550b4b15f6768552e3
Thanos <thanasakis@gmail.com>
2021-01-27 22:28:56 +0100

    Multiple providers at root level, wip cart provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/providers/cart_procider.dart';
 import './screens/product_details_screen.dart';
 import './screens/products_overview_screen.dart';
 import './providers/products_provider.dart';
 import 'package:provider/provider.dart';
-
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      // we are instantiating the provider, it is recommended to use the builder
-      // for efficiency.
-      create: (ctx) => ProductsProvider(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+            // we are instantiating the provider, it is recommended to use the builder
+            // for efficiency.
+            create: (ctx) => ProductsProvider()),
+        ChangeNotifierProvider(create: (ctx) => CartProvider()),
+      ],
       child: MaterialApp(
         title: 'MyShop',
         theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.deepOrange,
-          fontFamily: "Lato"
-        ),
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: "Lato"),
         home: ProductsOverviewScreen(),
         routes: {
           ProductDetailsScreen.routeName: (context) => ProductDetailsScreen()
--- lib/providers/cart_procider.dart
+++ lib/providers/cart_procider.dart
@@ -15,4 +15,27 @@ class CartItem {
 
 }
 
-class CartProvider with ChangeNotifier {}
+class CartProvider with ChangeNotifier {
+  Map<String, CartItem> _items;
+
+  Map<String, CartItem> get items {
+    return {...items};
+  }
+
+  void addItem(String productId, String title, double price) {
+    if (_items.containsKey(productId)) {
+      _items.update(productId, (existingItem) =>
+          CartItem(id: existingItem.id,
+              title: existingItem.title,
+              quantity: existingItem.quantity + 1,
+              price: existingItem.price))
+    } else {
+      _items.putIfAbsent(productId, () =>
+          CartItem(id: DateTime.now().toString(),
+              title: title,
+              quantity: 1,
+              price: price))
+    }
+  }
+
+}
