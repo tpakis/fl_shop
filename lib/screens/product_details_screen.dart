ea019c82494bf81056d5ac9cd2dfd2f8700b1e79
Thanos <thanasakis@gmail.com>
2021-02-03 23:25:20 +0100

    Added product details, softwrap for text

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -4,6 +4,7 @@ import '../providers/products_provider.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   static const routeName = "PRODUTC_DETAILS";
+
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
@@ -12,7 +13,42 @@ class ProductDetailsScreen extends StatelessWidget {
     final product = provider.findById(productId);
 
     return Scaffold(
-      appBar: AppBar(title: Text(product.title),),
+      appBar: AppBar(
+        title: Text(product.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.scaleDown,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "\$${product.price}",
+              style: TextStyle(color: Colors.grey, fontSize: 20),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10,),
+              width: double.infinity,
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
