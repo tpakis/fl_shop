6f60faa3307e3d5e0e2d61ac49c8c6b9c0e23844
Thanos <thanasakis@gmail.com>
2021-02-21 22:54:53 +0100

    handle http errors of patch, delete, optimistically update favorite

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -8,11 +9,33 @@ class Product with ChangeNotifier {
   final String imageUrl;
   bool isFavorite;
 
-  Product({@required this.id, @required this.title, @required this.description, @required this.price, @required this.imageUrl, this.isFavorite = false});
+  String get productUrl {
+    return "https://fl-shop-d7c4e-default-rtdb.firebaseio.com/products/$id.json";
+  }
 
-  void toggleFavoriteStatus() {
-    isFavorite = !isFavorite;
+  Product(
+      {@required this.id, @required this.title, @required this.description, @required this.price, @required this.imageUrl, this.isFavorite = false});
+
+  void _setFavoriteState(bool isFavorite) {
+    this.isFavorite = isFavorite;
     notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus() async {
+    // optimistically update value
+    final previousState = isFavorite;
+    _setFavoriteState(!isFavorite);
+    try {
+      var response = await http.patch(productUrl, body: json.encode({"isFavorite": isFavorite}));
+      // http package throws error only for get and post, not for patch, delete, so we manually check the
+      // response code for errors.
+      if (response.statusCode >= 400) {
+        _setFavoriteState(previousState);
+      }
+    // other network issues might occur though which will throw errors so we catch them also
+    } catch (error) {
+      _setFavoriteState(previousState);
+    }
   }
 }
 
