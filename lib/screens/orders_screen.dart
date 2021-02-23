5497076416cc71fc900fac8a03172d831ae12d0d
Thanos <thanasakis@gmail.com>
2021-02-23 21:38:18 +0100

    use FutureBuilder with consumer and backing future property
    Avoid infinite loop, avoid managing loading state

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_complete_guide/widgets/order_item.dart';
 import 'package:provider/provider.dart';
+import '../widgets/order_item.dart';
 import '../providers/orders_provider.dart';
 
 class OrdersScreen extends StatefulWidget {
@@ -11,41 +11,50 @@ class OrdersScreen extends StatefulWidget {
 }
 
 class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
+  Future _ordersFuture;
+
+  Future _getOrdersFuture() {
+    return Provider.of<OrdersProvider>(context, listen: false)
+        .fetchAndSetOrders();
+  }
 
   @override
   void initState() {
-    // hack because context is not available on init state or Future.delayed(Duration.zero).then(use provider)
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<OrdersProvider>(context, listen: false)
-          .fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
+    _ordersFuture = _getOrdersFuture();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    final ordersData = Provider.of<OrdersProvider>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text("Your Orders"),
       ),
-      body: (_isLoading)
-          ? Center(
+      body: FutureBuilder(
+        // use backing property so if the build method runs again we don't create new future
+        // We also avoid infinite loop by using a consumer on only the interested widget and listen false
+        future: _ordersFuture,
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
               child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: ordersData.orders.length,
-              itemBuilder: (ctx, index) {
-                return OrderItem(ordersData.orders[index]);
-              },
-            ),
+            );
+          } else if (dataSnapshot.hasError) {
+            return Center(
+              child: Text("An error occurred"),
+            );
+          } else {
+            return Consumer<OrdersProvider>(
+              builder: (ctx, ordersData, child) => ListView.builder(
+                itemCount: ordersData.orders.length,
+                itemBuilder: (ctx, index) {
+                  return OrderItem(ordersData.orders[index]);
+                },
+              ),
+            );
+          }
+        },
+      ),
     );
   }
 }
